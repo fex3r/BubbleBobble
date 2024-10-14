54ae5459197a16484f100fa451d01ceb8d5d01bf
Federico Pizzuti <federicopizzuti03@gmail.com>
2024-10-14 23:06:39 +0200

    toString Fede
    cicicicicic

--- BubbleBobble/src/fede.java
+++ BubbleBobble/src/fede.java
@@ -2,8 +2,13 @@
 public class fede 
 {
 	String nome = "fede";
-	public fede()
+	public fede(String nome)
 	{
-		
+		this.nome = nome;
+	}
+	
+	public String toString() {
+		System.out.println("ciciciiccii");
+		return "cicicicici";
 	}
 }
