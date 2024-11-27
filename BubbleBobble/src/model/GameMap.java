920d7f83d33a5177aacca6560f0d14625a1e7b5c
Federico Pizzuti <federicopizzuti03@gmail.com>
2024-11-27 19:27:38 +0100

    tolti dei print inutili

--- BubbleBobble/src/model/GameMap.java
+++ BubbleBobble/src/model/GameMap.java
@@ -22,14 +22,6 @@ public class GameMap
 	{
 		this.initializeBlocks();
 		this.initializeGameMap();
-		for(int i = 0; i<map.length; i++)
-		{
-			for(int j = 0; j<map[0].length; j++)
-			{
-				System.out.print(map[i][j]);
-			}
-			System.out.println();
-		}
 	}
 	
 	
